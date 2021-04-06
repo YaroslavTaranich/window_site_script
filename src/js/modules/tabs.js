f080e1de4291dd26107af208aa42b95e922cc861
Yaroslav Taranich <Yaroslavtaranic@yandex.ru>
2021-04-06 15:16:23 +0300

    tabs script add

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,37 @@
+const tabs = (headerSelector, tabSecector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector),
+          tabs = document.querySelectorAll(tabSecector),
+          content = document.querySelectorAll(contentSelector);
+
+    function hideTabContent() {
+        content.forEach(i => {
+            i.style.display = "none";
+            
+        });
+        tabs.forEach(i => {
+            i.classList.remove(activeClass);
+        });
+    }
+
+    function showTabContent(i = 0) {
+        content[i].style.display = "block";
+        tabs[i].classList.add(activeClass);
+    }
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        if ( target && (target.classList.contains(tabSecector.replace(/\./, "")) || 
+            target.parentNode.classList.contains(tabSecector.replace(/\./, "")))) {
+            tabs.forEach((item, i) => {
+                if (target == item || target.parentNode ==item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+};
+
+export default tabs;
